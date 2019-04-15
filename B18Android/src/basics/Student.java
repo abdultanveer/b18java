a4308c95df3c9825a8ddad9f88c42eb55c91dda0
abdul <abdul.tanveer@gmail.com>
2019-04-16 00:38:07 +0530

    thread sync

--- B18Android/src/basics/Student.java
+++ B18Android/src/basics/Student.java
@@ -1,5 +1,5 @@
 package basics;
-class Student{  
+public class Student{  
 	int rollno;  
 	String name,course;  
 	float fee;  
@@ -12,12 +12,23 @@ class Student{
 		this(rollno,name,course);//reusing constructor  
 		this.fee=fee;  
 	}  
-	void display(){System.out.println(rollno+" "+name+" "+course+" "+fee);}  
-}  
-class TestThis7{  
+	void display(){
+		System.out.println(rollno+" "+name+" "+course+" "+fee);
+		int c =	add(10,20);
+		System.out.println("c="+c);
+		c= add(30,40);
+		c=   add(15,67);
+	}  
+	public int add(int a, int b) {
+		System.out.println("sum is "+(a+b));
+		return a+b;
+	}
 	public static void main(String args[]){  
 		Student s1=new Student(111,"ankit","java");  
 		Student s2=new Student(112,"sumit","java",6000f);  
 		s1.display();  
 		s2.display();  
-	}} 
+	}
+}  
+class TestThis7{  
+} 
